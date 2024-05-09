caeea31002eee12cb13905ce3121384b26e84283
Matt Yeldalo <matt.yeldalo@outlook.com>
2024-05-09 19:29:43 +1000

    did stuff

--- init.lua
+++ init.lua
@@ -172,6 +172,9 @@ vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- Set ; -> : for faster commands
+vim.keymap.set('n', ';', ':')
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
