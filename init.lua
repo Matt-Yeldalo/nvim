27178a3bc1d35261c88d42dde7e0b742306e6793
Matt Yeldalo <matman370@hotmail.com>
2024-06-21 21:30:37 +1000

    added

--- init.lua
+++ init.lua
@@ -81,10 +81,6 @@ vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
 -- KEYMAPS
 require 'custom.keymaps'
-
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
@@ -95,7 +91,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
-
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
@@ -398,7 +393,7 @@ require('lazy').setup({
       {
         '<leader>f',
         function()
-          require('conform').format { async = true, lsp_format = "fallback" }
+          require('conform').format { async = true, lsp_format = 'fallback' }
         end,
         mode = '',
         desc = '[F]ormat buffer',
