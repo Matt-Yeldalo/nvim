ca0fc62b1ec0de734aa387e9a52bc88bd6503ab7
Matt Yeldalo <matman370@hotmail.com>
2024-07-18 09:24:38 +1000

    added shfmt

--- lua/matt/plugins/conform.lua
+++ lua/matt/plugins/conform.lua
@@ -20,6 +20,7 @@ return {
       ruby = { "rubocop" },
       lua = { "stylua" },
       javascript = { { "prettierd", "prettier" } },
+      zsh = {"shfmt"}
     },
     -- Set up format-on-save
     -- format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
