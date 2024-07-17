899898601b5122d7f108113429079d9982c062ec
Matt Yeldalo <matman370@hotmail.com>
2024-07-18 08:03:41 +1000

    added plugins

--- lua/matt/plugins/theme.lua
+++ lua/matt/plugins/theme.lua
@@ -1,12 +1,5 @@
 return {
-  "scottmckendry/cyberdream.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {
-    transparent = true,
-    italic_comments = true,
-    extensions = {
-      telescope = true
-    }
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000,
+  opts = {}
   }
 }
