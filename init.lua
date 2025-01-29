3951562ac645f900666dd290b91f605b4804a58d
Matt Yeldalo <matman370@hotmail.com>
2025-01-29 23:41:59 +1100

    need to cheery pick ad5ab41122a0b84f27101f1b5e6e55a681f84b2f

--- init.lua
+++ init.lua
@@ -620,6 +620,9 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
+      endwise = {
+        enable = true,
+      },
       playground = {
         enable = true,
       },
@@ -629,8 +632,6 @@ require('lazy').setup({
       highlight = {
         enable = true,
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
