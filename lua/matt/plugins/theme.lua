e5e7b5cec9c23a5ac26923b6334a3daf7f392e56
Matt Yeldalo <matman370@hotmail.com>
2025-02-05 19:13:25 +1100

    theme

--- lua/matt/plugins/theme.lua
+++ lua/matt/plugins/theme.lua
@@ -1,12 +1,11 @@
 local theme = 'olimorris/onedarkpro.nvim'
 local mod = 'onedarkpro'
-local scheme = 'onedark'
-
+local scheme = 'onedark_dark'
 
 return {
   theme,
   priority = 1000,
   config = function()
-    vim.cmd('colorscheme onedark_dark')
-  end
+    vim.cmd('colorscheme ' .. scheme)
+  end,
 }
