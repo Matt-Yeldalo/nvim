fa4e828ff3126db98d7ec5d2c3a57d58b759f7c1
Matt Yeldalo <matman370@hotmail.com>
2025-03-05 20:37:11 +1100

    tiny

--- lua/matt/plugins/tiny-inline.lua
+++ lua/matt/plugins/tiny-inline.lua
@@ -4,5 +4,6 @@ return {
   priority = 1000, -- needs to be loaded in first
   config = function()
     require('tiny-inline-diagnostic').setup()
+    vim.diagnostic.config { virtual_text = false }
   end,
 }
