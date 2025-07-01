e5c9e8db8a807a09dde3f4ef796312154ef7ec01
Matt Yeldalo <matman370@hotmail.com>
2025-07-01 19:50:09 +1000

    something

--- lua/matt/plugins/copilot-chat.lua
+++ lua/matt/plugins/copilot-chat.lua
@@ -1,20 +1,26 @@
 return {
-  "CopilotC-Nvim/CopilotChat.nvim",
-  branch = "main",
+  'CopilotC-Nvim/CopilotChat.nvim',
+  branch = 'main',
   dependencies = {
-    { "zbirenbaum/copilot.lua" }, -- required
-    { "nvim-lua/plenary.nvim" }, -- required
+    { 'zbirenbaum/copilot.lua' }, -- required
+    { 'nvim-lua/plenary.nvim' }, -- required
   },
   config = function()
-    require("CopilotChat").setup({
+    require('CopilotChat').setup {
+      mappings = {
+        reset = {
+          normal = '',
+          insert = '',
+        },
+      },
       show_help = true,
       prompts = {
-        Explain = "Explain how this code works.",
-        Fix = "Fix this code.",
-        Review = "Review this code.",
-        Tests = "Suggest tests for this code.",
-        Refactor = "Refactor this code to improve clarity."
+        Explain = 'Explain how this code works.',
+        Fix = 'Fix this code.',
+        Review = 'Review this code.',
+        Tests = 'Suggest tests for this code.',
+        Refactor = 'Refactor this code to improve clarity.',
       },
-    })
-  end
+    }
+  end,
 }
