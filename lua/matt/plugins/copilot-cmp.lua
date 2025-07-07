15d1a902a9f426d201efb7428c1eb740feb14e88
Matt Yeldalo <matman370@hotmail.com>
2025-07-07 22:55:33 +1000

    something

--- lua/matt/plugins/copilot-cmp.lua
+++ lua/matt/plugins/copilot-cmp.lua
@@ -16,7 +16,6 @@ return {
     vim.g.copilot_hide_during_completion = false
     vim.g.copilot_proxy_strict_ssl = false
     vim.g.copilot_settings = { selectedCompletionModel = 'claude-3.5-sonnet' }
-    vim.keymap.set('i', '<S-Tab>', '<cmd>Copilot suggestion accept<CR>', { expr = true, replace_keycodes = false })
     vim.keymap.set('n', '<leader>cp', '<cmd>CopilotChatOpen<CR>', {})
   end,
 }
