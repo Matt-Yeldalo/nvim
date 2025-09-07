1e3cd25e9e2371070e618990e6cc48e874401a7c
Matt Yeldalo <matman370@hotmail.com>
2025-09-07 19:12:28 +1000

    tab managanement

--- lua/matt/plugins/bufferline.lua
+++ lua/matt/plugins/bufferline.lua
@@ -2,15 +2,29 @@ return {
   'akinsho/bufferline.nvim',
   after = 'catppuccin',
   config = function()
+    vim.keymap.set('n', '<leader>tn', '<Cmd>BufferLineCycleNext<CR>', { desc = 'Next tab' })
+    vim.keymap.set('n', '<leader>tp', '<Cmd>BufferLineCyclePrev<CR>', { desc = 'Previous tab' })
+    vim.keymap.set('n', '<leader>tc', '<Cmd>bd<CR>', { desc = 'Close tab' })
+    vim.keymap.set('n', '<leader>t<', '<Cmd>BufferLineMovePrev<CR>', { desc = 'Move tab left' })
+    vim.keymap.set('n', '<leader>t>', '<Cmd>BufferLineMoveNext<CR>', { desc = 'Move tab right' })
     local macchiato = require('catppuccin.palettes').get_palette 'macchiato'
     require('bufferline').setup {
       highlights = require('catppuccin.groups.integrations.bufferline').get_theme {
         styles = { 'italic', 'bold' },
         custom = {
-          macchiato = {
-            fill = { bg = macchiato.surface0 },
-            background = { bg = macchiato.surface0 },
-          },
+          -- macchiato = {
+          --   fill = { bg = macchiato.surface0 },
+          --   background = { bg = macchiato.surface0 },
+          -- },
+        },
+        always_show_bufferline = true,
+        show_close_icon = true,
+        show_tab_indicators = true,
+        separator_style = 'thick',
+        hover = {
+          enabled = true,
+          delay = 120,
+          reveal = { 'close' },
         },
       },
     }
--- lua/matt/plugins/copilot-chat.lua
+++ lua/matt/plugins/copilot-chat.lua
@@ -13,7 +13,7 @@ return {
           insert = '',
         },
       },
-      model = 'claude-3.5-sonnet',
+      model = 'claude-4-sonnet',
       show_help = true,
       prompts = {
         Explain = 'Explain how this code works.',
